d09a20940c541ca8338afcdc4508fe4421469bb3
Beyza Alkış <66977215+beyzaalks@users.noreply.github.com>
2020-12-22 10:50:18 +0300

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,26 @@
+#substring
+
+message = "Hello World."
+
+print(message[2:6])
+
+
+new_message = "Good Luck"
+new_message_sub = new_message[2:6]
+print(new_message_sub)
+
+
+new_message2 = "Nice Day"
+new_message_sub2 = new_message2[1:]
+print(new_message_sub2)
+
+
+new_message3 = "Nice Day"
+new_message_sub3 = new_message3[:1]
+print(new_message_sub3)
+
+
+print(len(message))
+
+
+print(message[len(message)-1:len(message)])
