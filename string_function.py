03b2e7d9a5b57b2412c614c3dde07fc1e2d3cd6f
Beyza Alkış <66977215+beyzaalks@users.noreply.github.com>
2020-12-22 10:50:01 +0300

    Add files via upload

---
+++ string_function.py
@@ -0,0 +1,12 @@
+# first string
+first_string = "python is awesome!"
+
+# second string
+second_string = "PyThOn Is AwEsOmE!"
+
+if(first_string == second_string):
+    print("The strings are same.")
+else:
+    print("the strings are not same.")    
+    
+    
